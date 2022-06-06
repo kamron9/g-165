9d72e1b2a422dc92e33eaac44c5f177358a077b5
Nikto711 <nikto711nikto@gmail.com>
2022-06-06 18:00:11 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+/* primitive
+ number  > raqam 1234
+ string  > matn '', "",``
+ boolean > true yoki false
+ undefined > belgilanmagan
+ null > 0 
+ symbol > unique words
+ bigInt > katta hajmdagi sonlarni saqlash uchun
+*/
+
+
+/* non-primitive, reference, object 
+ array > []
+ object > {}
+ function > funksiya
+*/
+
+
+
+
+
+
+
+
+
+//Math.round
+//Math.pow
+//Math.sqrt
+//Math.cbrt
+//Math.max
+//Math.min
+//Math.floor
+//Math.ceil
+//Math.random
+//Math.abs
+
+
+//comparision operators
+
+
+//string
+// '', "", ``
+
+//length
+//indexOf
+//search
+//lastIndexOf
+//startsWith
+//endsWith
+//charAt
+//charCodeAt
+//concat
+//includes
+//repeat
+//replace
+//slice
+//substring
+//substr
+//split
+//match
+//toLowerCase
+//toUpperCase
+
+
+
+
